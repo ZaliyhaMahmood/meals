276f49ce52b859783a8af51b1f57b3e15a92c297
zaliyha <zaliyhamahmood@flexisaf.com>
2022-11-13 14:10:42 +0100

    implement restaurant screen view

--- App.js
+++ App.js
@@ -1,28 +1,12 @@
 import React from "react";
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
-import { StyleSheet, Text, SafeAreaView, View, StatusBar } from "react-native";
+import RestaurantsScreen from "./src/features/restaurants/screens/restaurants.screen";
 
 export default function App() {
   return (
     <>
-      <SafeAreaView style={styles.container}>
-        <View style={styles.search}>
-          <Text>search</Text>
-        </View>
-        <View style={styles.list}>
-          <Text>list</Text>
-        </View>
-      </SafeAreaView>
+      <RestaurantsScreen />
       <ExpoStatusBar style="auto" />
     </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginTop: StatusBar.currentHeight,
-  },
-  search: { backgroundColor: "green", padding: 16 },
-  list: { flex: 1, backgroundColor: "blue", padding: 16 },
-});
