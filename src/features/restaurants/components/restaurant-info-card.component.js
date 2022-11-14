8656112c796dec7bef4c37a4a8c4727c7226efb5
zaliyha <zaliyhamahmood@flexisaf.com>
2022-11-14 13:50:07 +0100

    migrate to styled-components

--- src/features/restaurants/components/restaurant-info-card.component.js
+++ src/features/restaurants/components/restaurant-info-card.component.js
@@ -1,6 +1,20 @@
 import React from "react";
 import { StyleSheet, Text, SafeAreaView, View, StatusBar } from "react-native";
 import { Card } from "react-native-paper";
+import styled from "styled-components/native";
+
+const RestaurantCard = styled(Card)`
+  background-color: white;
+`;
+
+const RestaurantCardCover = styled(Card.Cover)`
+  padding: 20px;
+  background-color: white;
+`;
+
+const Title = styled.Text`
+  padding: 16px;
+`;
 
 const RestaurantInfoCard = ({ restaurant = {} }) => {
   const {
@@ -16,17 +30,17 @@ const RestaurantInfoCard = ({ restaurant = {} }) => {
   } = restaurant;
 
   return (
-    <Card elevation={5} style={styles.card}>
-      <Card.Cover key={name} style={styles.cover} source={{ uri: photos[0] }} />
-      <Text style={styles.title}>{name}</Text>
-    </Card>
+    <RestaurantCard elevation={5}>
+      <RestaurantCardCover key={name} source={{ uri: photos[0] }} />
+      <Title>{name}</Title>
+    </RestaurantCard>
   );
 };
 
 export default RestaurantInfoCard;
 
-const styles = StyleSheet.create({
-  card: { backgroundColor: "white" },
-  cover: { padding: 20, backgroundColor: "white" },
-  title: { padding: 16 },
-});
+// const styles = StyleSheet.create({
+//   card: { backgroundColor: "white" },
+//   cover: { padding: 20, backgroundColor: "white" },
+//   title: { padding: 16 },
+// });
